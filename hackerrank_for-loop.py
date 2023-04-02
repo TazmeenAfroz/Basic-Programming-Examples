f5cd113a2ba189469ef452f5a13b3e5b47b1311a
tazmeen <p229252@pwr.nu.edu.pk>
2023-04-02 10:44:27 +0500

    For-Loop problem

---
+++ hackerrank_for-loop.py
@@ -0,0 +1,7 @@
+'''Task
+The provided code stub reads and integer, n, from STDIN. For all non-negative integers i< n, print . n^2'''
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(0,n):
+      print(i*i)
+
