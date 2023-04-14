83283eee2a600d2e1654a28986a1f4346a601e2b
tazmeen <p229252@pwr.nu.edu.pk>
2023-04-14 22:02:27 +0500

    Python lambda Function with an Argument

---
+++ lambda_function.py
@@ -0,0 +1,12 @@
+
+Text_message = lambda message : print("Hello!", message)
+
+Personal_info = lambda info : print("My name is ", info)
+
+
+
+if __name__ == "__main__":
+
+    Text_message("How are you?")
+
+    Personal_info("Tazmeen Afroz")
