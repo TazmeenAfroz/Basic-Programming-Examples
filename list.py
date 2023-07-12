11519b1504a533a62cade4b9cebf1495b8119a4e
Tazmeen Afroz <p229252@pwr.nu.edu.pk>
2023-07-12 15:01:07 +0500

    Add files via upload

--- list.py
+++ list.py
@@ -1,20 +1,19 @@
-name = input("Enter the name of student: ")
-age = input("Enter age: ")
-cgpa = input("Enter CGPA: ")
-
-student_info = {"name": name, "age": age, "cgpa": cgpa}
-
-# Printing name without brackets
-print(student_info["name"])
-
-# Printing the whole dictionary
-print(student_info)
 
 
-# Printing all items without brackets
-for key, value in student_info.items():
-    print(key + ":", value)
-   
-# Creating a list of dictionary keys and accessing a specific key
-keys_list = list(student_info.keys())
-print(keys_list[0])  # Prints "name"
+numberlist = list()
+
+while True:
+
+    num = input("Enter number and enter exit to quit :")
+    if num == 'exit':
+      break
+
+    numberlist.append(float(num))
+    
+print("Maximum:",max(numberlist))
+print("Minimum:",min(numberlist))
+print("Sum :",sum(numberlist))
+print("Average:",sum(numberlist)/len(numberlist))
+print("Total numbers entered:",len(numberlist))
+
+  
